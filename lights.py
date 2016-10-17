89838821c6b972984d8d94a26b9c436b7abc9a0e
Dequisitor <enantiodromy@gmail.com>
2016-10-17 21:47:32 +0100

    - fix for subprocess calls

--- lights.py
+++ lights.py
@@ -2,9 +2,9 @@ import subprocess
 import time
 
 def main(self):
-    subprocess('irsend SEND_ONCE RGBLED power', shell=True)
+    subprocess.call('irsend SEND_ONCE RGBLED power', shell=True)
     time.sleep(3)
-    subprocess('irsend SEND_ONCE RGBLED white0', shell=True)
+    subprocess.call('irsend SEND_ONCE RGBLED white0', shell=True)
 
 if __name__ == '__main__':
     main()
